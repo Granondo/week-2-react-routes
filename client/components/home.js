e9a6916bd32f1cab76042431f6d12fb950e4fe1e
Granondo <68280035+Granondo@users.noreply.github.com>
2020-10-15 19:47:38 +0300

    2end task is finished

--- client/components/home.js
+++ client/components/home.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Route, Switch} from 'react-router-dom'
 import Header from './header'
+import Dashboard from './dashboard'
+import Main from './main'
+import Profile from './profile'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen"> 
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          {/* add your routes here */}
+          <Switch>
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <Profile />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+          </Switch>
         </div>
       </div>
     </div>
