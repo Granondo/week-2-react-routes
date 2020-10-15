770ba8b1aac3c9414ef12bc11af3f1cf004a5822
Granondo <68280035+Granondo@users.noreply.github.com>
2020-10-15 20:12:03 +0300

    profile updated

--- client/components/profile.js
+++ client/components/profile.js
@@ -10,7 +10,8 @@ const Profile = () => {
         <Head title="Hello" />
         <div className="flex items-center justify-center h-screen">
           <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-            <div id="title">Profile {user} </div>
+            <div id="title">Profile</div>
+            <div id="username"> {user} </div>
             <Link to="/dashboard">Go To Root</Link>
             <Link to="/dashboard/main">Go To Main</Link>
           </div>
